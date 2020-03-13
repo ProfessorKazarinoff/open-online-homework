df91ab5bc70f984f5645c176ea9e992953070dcc
professorkazarinoff <peter.kazarinoff@pcc.edu>
2020-03-12 21:55:08 -0700

    added pages app started homepage and about page

--- src/courses/models.py
+++ src/courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     code = models.CharField(max_length=10)
 
     def __str__(self):
--- src/open_online_homework/settings.py
+++ src/open_online_homework/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'open_online_homework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
