912401327b83a6128e613ab6d78491c299eb498d
professorkazarinoff <peter.kazarinoff@pcc.edu>
2020-03-15 10:19:26 -0700

    moved back to local sqlite database

--- open_online_homework/settings.py
+++ open_online_homework/settings.py
@@ -83,13 +83,14 @@ WSGI_APPLICATION = 'open_online_homework.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 """
 # free elphant db hosted databse good elphant db.com log in with google
 DATABASES = {
@@ -102,6 +103,7 @@ DATABASES = {
         'Port': '5432'
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
