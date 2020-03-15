a6c8a105579393b4f7fbed5982dc784c1aa0ccec
professorkazarinoff <peter.kazarinoff@pcc.edu>
2020-03-15 10:13:42 -0700

    add elephant db hosted database

--- open_online_homework/settings.py
+++ open_online_homework/settings.py
@@ -83,14 +83,25 @@ WSGI_APPLICATION = 'open_online_homework.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
+# free elphant db hosted databse good elphant db.com log in with google
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kpxxcgsm',
+        'USER': 'kpxxcgsm',
+        'PASSWORD': 'xuEdFAdcOdypE2bnbN7oRmd3gwYaMwbN',
+        'HOST': 'drona.db.elephantsql.com',
+        'Port': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
